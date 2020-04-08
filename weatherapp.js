f89de05583baef27f72c0ba8d8886be23b161810
Christin <christin-thoma@t-online.de>
2020-04-07 20:53:59 -0600

    Adding fetch to weather app

---
+++ weatherapp.js
@@ -0,0 +1,27 @@
+function getWeather(woeid) {
+    fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`)
+        .then(result => {
+            //console.log(result);
+            // look at the body under the response which is a readable stream. We can convert that to
+            // JS object. json() will return a promise, because this method can take some time to run
+            // in order to not stop the code it is implemented async.
+            return result.json();
+        })
+        .then(data => {
+            const today = data.consolidated_weather;
+            let d = new Date().getDay();
+            d = parseInt(d)
+            console.log(d)
+            var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+            for (var i = 0; i < today.length; i++) {
+                // console.log(data)
+                console.log(`Temperature ${days[d + i]} in ${data.title} will be maximum ${today[i].max_temp}C and minimum ${today[i].min_temp}C`)
+            }
+        })
+
+        .catch(error => {
+            console.log(error);
+        });
+}
+
